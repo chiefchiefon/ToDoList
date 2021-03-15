e1bc5044c7243ea468324d355a0e157ad2ad0979
= <shaul.chamoula@mail.huji.ac.il>
2021-03-15 10:14:17 +0200

    add ToDoListViewData to ToDoItemViewData.kt

--- app/src/main/java/com/github/chiefchiefon/todolist/viewmodel/ToDoItemViewData.kt
+++ app/src/main/java/com/github/chiefchiefon/todolist/viewmodel/ToDoItemViewData.kt
@@ -1,3 +1,15 @@
 package com.github.chiefchiefon.todolist.viewmodel
 
 data class ToDoItemViewData(val id: Int?, val itemText: String, val isCompleted: Boolean)
+
+// Orel's code
+
+//data class ToDoItemViewData(
+//    val id: Int,
+//    val text: String,
+//    val isDone: Boolean
+//)
+
+data class ToDoListViewData (
+    val itemsList: List<ToDoItemViewData> = emptyList()
+        )
