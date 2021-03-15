3c43d58f56a0a5fdb9fc7dc9e840de5562fe8f74
= <shaul.chamoula@mail.huji.ac.il>
2021-03-15 12:26:09 +0200

    correct mistake: ViewModelProvider.Factory instead of ViewModelProvider in ToDoViewModelFactory.kt

--- app/src/main/java/com/github/chiefchiefon/todolist/viewmodel/ToDoViewModelFactory.kt
+++ app/src/main/java/com/github/chiefchiefon/todolist/viewmodel/ToDoViewModelFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.github.chiefchiefon.todolist.model.db.ToDoRepository
 
-class ToDoViewModelFactory(private val repository: ToDoRepository) : ViewModelProvider {
+class ToDoViewModelFactory(private val repository: ToDoRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return ToDoViewModel(repository) as T
     }
