aa8d8541c80f0bbf80b9358002666ea51c76782c
= <shaul.chamoula@mail.huji.ac.il>
2021-03-16 09:42:13 +0200

    comment fixing

--- app/src/main/java/com/github/chiefchiefon/todolist/model/db/ToDoItem.kt
+++ app/src/main/java/com/github/chiefchiefon/todolist/model/db/ToDoItem.kt
@@ -6,10 +6,11 @@ import androidx.room.PrimaryKey
 @Entity
 data class ToDoItem (
     /**
-     * We'llalways use id, because it accelerate the access to DB
+     * We'll always use id, because it accelerate the access to DB
      */
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null,
+
     val itemText: String,
     val isCompleted: Boolean = false,
     val priority: Int
