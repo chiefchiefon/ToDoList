4bd00105cce0d2a0b4f85916463db77f28095081
= <shaul.chamoula@mail.huji.ac.il>
2021-03-16 09:50:21 +0200

    complete MainActivity.kt

--- app/src/main/java/com/github/chiefchiefon/todolist/MainActivity.kt
+++ app/src/main/java/com/github/chiefchiefon/todolist/MainActivity.kt
@@ -2,19 +2,49 @@ package com.github.chiefchiefon.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.room.Room
-import com.github.chiefchiefon.todolist.model.AppDataBase
-import com.github.chiefchiefon.todolist.model.ToDoItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.github.chiefchiefon.todolist.model.db.RoomCreator
+import com.github.chiefchiefon.todolist.model.db.ToDoRepository
+import com.github.chiefchiefon.todolist.view.ToDoListAdapter
+import com.github.chiefchiefon.todolist.viewmodel.ToDoItemViewData
+import com.github.chiefchiefon.todolist.viewmodel.ToDoViewModel
+import com.github.chiefchiefon.todolist.viewmodel.ToDoViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<ToDoViewModel> {
+        val toDoDao = RoomCreator
+            .getDb(this)
+            .toDoDao()
+        ToDoViewModelFactory(ToDoRepository(toDoDao))
+    }
+
+    private val toDoListAdapter by lazy { ToDoListAdapter(this::onTodoChanged) }
+
+    private fun onTodoChanged(toDoItemViewData: ToDoItemViewData) {
+        viewModel.onToDoChanged(toDoItemViewData)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = Room.databaseBuilder(this, AppDataBase::class.java, "app-db")
-            .allowMainThreadQueries()
-            .build()
+        val toDoListView: RecyclerView = findViewById(R.id.itemsList)
+        toDoListView.adapter = toDoListAdapter
 
-        db.toDoDao().insert(ToDoItem(null, "Learn SQL", false))
+        viewModel.listLiveData.observe(this) { viewState ->
+            toDoListAdapter.submitList(viewState.itemsList)
+        }
+
+        val addItemEditText: EditText = findViewById(R.id.addItemET)
+        val addItemBtn: Button = findViewById(R.id.addItemBtn)
+
+        addItemBtn.setOnClickListener {
+            viewModel.onItemClicked(addItemEditText.text.toString())
+            addItemEditText.text = null
+        }
     }
 }
